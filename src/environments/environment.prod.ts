8d7cbb11cd91091d7fab9409e5984bfcb5722e2d
leandrocodegit <lpoliveira.ti@gmail.com>
2024-11-02 19:46:22 -0300

    Adicionado mapa e pesquisa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ import { HttpHeaders } from "@angular/common/http";
 
 export const environment = {
   production: true,
-  urlApi: 'http://localhost:8000',
-  urlWebSocket: 'ws://localhost:8000/ws',
+  urlApi: 'vps55601.publiccloud.com.br:8000',
+  urlWebSocket: 'ws://vps55601.publiccloud.com.br:8000/ws',
   headers: {
     headers: new HttpHeaders({
-      "Content-Type": "application/json" 
+      "Content-Type": "application/json"
     })
   },
 };
