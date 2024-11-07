f8c214fcd2879b395f1e2a23893f024a1bc1f4f4
leandrocodegit <lpoliveira.ti@gmail.com>
2024-11-07 14:08:16 -0300

    Adicionado detalhes dispositivo

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,9 +1,9 @@
 import { HttpHeaders } from "@angular/common/http";
 
 export const environment = {
-  production: true,
-  urlApi: 'http://vps55601.publiccloud.com.br:8000',
-  urlWebSocket: 'ws://vps55601.publiccloud.com.br:8000/ws',
+  production: false,
+  urlApi: 'http://localhost:8000',
+  urlWebSocket: 'ws://localhost:8000/ws',
   headers: {
     headers: new HttpHeaders({
       "Content-Type": "application/json"
