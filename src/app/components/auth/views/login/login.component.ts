b79ae5c608726b07e8067d3775493146bccda228
leandrocodegit <lpoliveira.ti@gmail.com>
2024-11-20 20:21:53 -0300

    Novo modelo

--- src/app/components/auth/views/login/login.component.ts
+++ src/app/components/auth/views/login/login.component.ts
@@ -55,6 +55,7 @@ export class LoginComponent {
       this.isLoad = false;
       this.router.navigate([`/dashboard`]);
     }, fail => {
+      console.log(fail);
       this.isError = true;
       this.isLoad = false;
     });
