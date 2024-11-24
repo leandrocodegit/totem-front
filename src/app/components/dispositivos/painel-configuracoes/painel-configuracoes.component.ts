e8d8ab6f2490418ded026147b717fb0008d24fe0
leandrocodegit <lpoliveira.ti@gmail.com>
2024-11-24 00:06:17 -0300

    Criando compose

--- src/app/components/dispositivos/painel-configuracoes/painel-configuracoes.component.ts
+++ src/app/components/dispositivos/painel-configuracoes/painel-configuracoes.component.ts
@@ -120,6 +120,9 @@ export class PainelConfiguracoesComponent implements OnInit, OnDestroy {
 
   onSliderChange() {
     if (this.enviarConfiguracao.value) {
+      console.log("Format: ", this.dispositivo.cor.cor);
+      console.log("Format: ", this.dispositivoService.formatCor(this.dispositivo.cor.cor, this.dispositivo.configuracao.tipoCor));
+
       this.mqttSevice.unsafePublish(`device/receive/${this.dispositivo.mac}`, `{
         "efeito": "${this.dispositivo.cor.efeito}",
         "cor": [${this.dispositivoService.formatCor(this.dispositivo.cor.cor, this.dispositivo.configuracao.tipoCor)}],
