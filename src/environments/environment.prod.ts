451f4ede4e53c39d0fac080c786e600cb0bea161
leandrocodegit <lpoliveira.ti@gmail.com>
2024-11-28 15:44:39 -0300

    Criando compose

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ import { HttpHeaders } from "@angular/common/http";
 
 export const environment = {
   production: true,
-  urlApi: 'http://http://169.150.1.85:8000/totem',
-  urlbroker: 'http://http://169.150.1.85:8080',
-  urlWebSocket: 'ws://http://169.150.1.85:8080/ws',
+  urlApi: 'http://169.150.1.85:8000/totem',
+  urlbroker: 'http://169.150.1.85:8080',
+  urlWebSocket: 'ws://169.150.1.85:8080/ws',
   headers: {
     headers: new HttpHeaders({
       "Content-Type": "application/json"
