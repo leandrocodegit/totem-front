bca4141e106802c036fee3596a26981d73062171
leandrocodegit <lpoliveira.ti@gmail.com>
2024-12-02 07:59:55 -0300

    Criando dark

--- src/app/components/dispositivos/detalhes-dispositivo/detalhes-dispositivo.component.ts
+++ src/app/components/dispositivos/detalhes-dispositivo/detalhes-dispositivo.component.ts
@@ -2,13 +2,15 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { Dispositivo } from '../../models/dispositivo.model';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-detalhes-dispositivo',
   standalone: true,
   imports: [
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatCardModule
   ],
   templateUrl: './detalhes-dispositivo.component.html',
   styleUrl: './detalhes-dispositivo.component.scss'
