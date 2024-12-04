bfaf6e6a52ab1683aff64382b5305f3018677932
leandrocodegit <lpoliveira.ti@gmail.com>
2024-12-04 16:31:23 -0300

    Criando dark

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ import { HttpHeaders } from "@angular/common/http";
 
 export const environment = {
   production: true,
-  //urlApi: 'http://169.150.1.85:8000/totem',
-  //urlbroker: 'http://169.150.1.85:8080',
-  //urlWebSocket: '169.150.1.85',
-  urlApi: 'http://vps55601.publiccloud.com.br:8000/totem',
-  urlbroker: 'http://vps55601.publiccloud.com.br:8080',
-  urlWebSocket: 'vps55601.publiccloud.com.br',
+  urlApi: 'http://169.150.1.85:8000/totem',
+  urlbroker: 'http://169.150.1.85:8080',
+  urlWebSocket: '169.150.1.85',
+  //urlApi: 'http://vps55601.publiccloud.com.br:8000/totem',
+  //urlbroker: 'http://vps55601.publiccloud.com.br:8080',
+  //urlWebSocket: 'vps55601.publiccloud.com.br',
   headers: {
     headers: new HttpHeaders({
       "Content-Type": "application/json"
