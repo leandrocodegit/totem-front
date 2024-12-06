c1367aab6c354c63d015bbdc15f2407551fc8d95
leandrocodegit <lpoliveira.ti@gmail.com>
2024-12-06 19:27:59 -0300

    Criando dark

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -5,9 +5,9 @@ export const environment = {
   //urlApi: 'http://169.150.1.85:8000/totem',
   //urlbroker: 'http://169.150.1.85:8080',
   //urlWebSocket: 'ws://169.150.1.85:9001/ws',
-  urlApi: 'http://vps55601.publiccloud.com.br:8000/totem',
+  //urlApi: 'http://vps55601.publiccloud.com.br:8000/totem',
   //urlbroker: 'http://vps55601.publiccloud.com.br:8080',
-  //urlApi: 'http://localhost:8081/totem',
+  urlApi: 'http://localhost:8081/totem',
   urlbroker: 'http://localhost:8082',
   urlWebSocket: 'vps55601.publiccloud.com.br',
   headers: {
