d9be2e6ab09c6842cc3ef8e7bb22ed5479aea711
leandrocodegit <lpoliveira.ti@gmail.com>
2024-12-07 21:30:55 -0300

    Criando dark

--- src/app/components/dispositivos/atualizar-firmware/atualizar-firmware.component.ts
+++ src/app/components/dispositivos/atualizar-firmware/atualizar-firmware.component.ts
@@ -26,7 +26,7 @@ export class AtualizarFirmwareComponent implements OnInit {
 
   @Input() dispositivo?: Dispositivo;
   protected selectedFile: File | null = null;
-  protected payload: any;
+  protected mensagens: string[] = [];
   private atualizarPayload = false;
 
   constructor(
@@ -35,56 +35,72 @@ export class AtualizarFirmwareComponent implements OnInit {
     private readonly mqttSevice: MqttService,
   ) {
 
-   /*  comandoService.firmwareEmit.subscribe(data => {
+    /*  comandoService.firmwareEmit.subscribe(data => {
 
-      if (data) {
-        if (data.includes('não') || data.toUpperCase().includes('FALHA')) {
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Falha',
-            detail: 'Falha ao atualizar'
-          });
-        }
-        else if (data.includes('Online')) {
-          this.messageService.add({
-            severity: 'success',
-            summary: 'Firmware atualizado com sucesso',
-            detail: data
-          });
-        }
-      }
-    }) */
+       if (data) {
+         if (data.includes('não') || data.toUpperCase().includes('FALHA')) {
+           this.messageService.add({
+             severity: 'error',
+             summary: 'Falha',
+             detail: 'Falha ao atualizar'
+           });
+         }
+         else if (data.includes('Online')) {
+           this.messageService.add({
+             severity: 'success',
+             summary: 'Firmware atualizado com sucesso',
+             detail: data
+           });
+         }
+       }
+     }) */
   }
 
   ngOnInit(): void {
     this.mqttSevice.observe(`device/confirmacao/a0:a3:b3:76:dc:37`).subscribe((message: any) => {
-      console.log('Mensagens', message.payload);
       if (message.payload instanceof Uint8Array) {
         const decoder = new TextDecoder('utf-8');
         const decodedPayload = decoder.decode(message.payload);
 
-        console.log('Payload decodificado:', decodedPayload);
+        console.log('Payload decodificado:', this.atualizarPayload);
+
 
         try {
 
           const response = JSON.parse(decodedPayload);
-          if(this.atualizarPayload && response.comando != 'ACEITO')
-            this.payload = JSON.parse(decodedPayload);
+          console.log(response);
 
-          if(this.payload.comando == 'FALHA', this.payload.comando == 'ATUALIZADO_FIRMWARE'){
-            this.atualizarPayload = false;
+          if (response.comando != 'ACEITO')
+            if(this.getMessagem(response.comando))
+              this.mensagens.push(this.getMessagem(response.comando));
+          if (response.comando != 'ATUALIZADO_FIRMWARE'){
+            this.atualizarPayload = true;
+            this.mensagens.push('Atualização foi concluída')
           }
+           if (response.comando == 'ACEITO' && this.atualizarPayload){
+            this.mensagens.push('Nova versão ' + response.versao)
+            this.atualizarPayload = false;
 
+          }
 
         } catch (e) {
         }
-    } else {
-    }
-
-      if (message) {
-        this.payload.data = message.comando;
       }
     });
+  }
+
+  getMessagem(comando: string){
+
+    switch(comando){
+      case 'FALHA': return 'Falha ao atualizar';
+      case 'DOWNLOAD': return 'Fazendo download do arquivo';
+      case 'DOWNLOAD_OK': return 'Download foi concluido';
+      case 'ATUALIZANDO_FIRMWARE': return 'Fazendo atualização do firmware';
+      case 'ATUALIZADO_FIRMWARE': return 'Firmware foi atualizado com sucesso';
+    }
+
+    return '';
+
   }
 
   onFileSelected(event: Event): void {
@@ -104,15 +120,13 @@ export class AtualizarFirmwareComponent implements OnInit {
 
         if (this.dispositivo?.mac && response.id) {
           this.atualizarPayload = true;
+          this.mensagens.push('Iniciando atualização')
           this.comandoService.updateFirmware(this.dispositivo?.mac).subscribe({
             complete: () => {
+              this.mensagens.push('Iniciando atualização')
             },
             error: (err) => {
-              this.messageService.add({
-                severity: 'error',
-                summary: 'Falha',
-                detail: 'Falha ao atualizar'
-              });
+
             }
           });
         }
