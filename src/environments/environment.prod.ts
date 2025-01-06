b2fb5525dd9eff727555bd4f46df30d4a30ef349
leandrocodegit <lpoliveira.ti@gmail.com>
2025-01-06 12:47:04 -0300

    Criando dark

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { HttpHeaders } from "@angular/common/http";
 
 export const environment = {
   production: true,
-  urlApi: 'http://sincroled.com.br:8000/totem',
+  urlApi: 'https://sincroled.com.br:8000/totem',
   urlbroker: 'http://sincroled.com.br:8080',
   urlWebSocket: 'sincroled.com.br',
   headers: {
