ca97165bff8cbbe77ec94c278669c5445cb67e5e
leandrocodegit <lpoliveira.ti@gmail.com>
2025-01-06 10:57:56 -0300

    Criando dark

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ import { HttpHeaders } from "@angular/common/http";
 
 export const environment = {
   production: true,
-  urlApi: 'http://vps55601.publiccloud.com.br:8000/totem',
-  urlbroker: 'http://vps55601.publiccloud.com.br:8080',
+  urlApi: 'https://sincroled.com.br:8000/totem',
+  urlbroker: 'https://sincroled.com.br:8080',
   urlWebSocket: 'vps55601.publiccloud.com.br',
   headers: {
     headers: new HttpHeaders({
       "Content-Type": "application/json"
     })
-  } 
+  }
 };
