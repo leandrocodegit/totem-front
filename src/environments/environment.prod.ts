00441635906b3b62aae6d8bf08c650ff867ab0ae
leandrocodegit <lpoliveira.ti@gmail.com>
2025-01-09 21:28:45 -0300

    Criando dark

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: true,
   urlApi: 'https://sincroled.com.br:8000/totem',
   urlbroker: 'https://sincroled.com.br:8080',
-  urlWebSocket: 'vps55601.publiccloud.com.br',
+  urlWebSocket: 'sincroled.com.br',
   headers: {
     headers: new HttpHeaders({
       "Content-Type": "application/json"
