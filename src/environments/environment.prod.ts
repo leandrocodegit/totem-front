d235090242d89ad95ee024eb3af52a703e2e03d4
leandrocodegit <lpoliveira.ti@gmail.com>
2025-01-16 19:09:42 -0300

    Institucional

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import { HttpHeaders } from "@angular/common/http";
 
 const protocolo = 'https://';
-const host = 'sincroled.com.br';
+const host = 'painel.sincroled.com.br';
 const porta = ":9000"
 export const environment = {
   url: protocolo + host + porta,
